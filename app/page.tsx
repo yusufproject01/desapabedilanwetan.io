3411a45e7b12bbd8cc56d3ace97587f405f69db1
yusufproject01 <yusufproject01@gmail.com>
2025-07-17 09:55:07 +0700

    menambahkan lokasi dan jam kerja

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import HeroSection from "@/components/HeroSection";
 import Menu from "../layouts/quick-access/menu";
 import About from "@/content/about";
 import JadwalSholat from "@/components/Cards/Containers/jadwal-sholat";
+import MapCard from "@/components/Cards/Containers/MapCard";
+import JamKerjaCard from "@/components/Cards/Containers/JamKerjaCard";
+// import dynamic from "next/dynamic";
+
+// const MapCard = dynamic(() => import("@/components/Cards/Containers/MapCard"));
 
 export default function Home() {
   return (
@@ -12,10 +17,10 @@ export default function Home() {
       <HeroSection />
       <Menu />
       <About />
-      <section className="w-full h-auto flex sm:flex-row flex-col sm:justify-around items-center sm:gap-y-0 gap-y-4 sm:px-4 px-2">
+      <section className="w-full h-auto flex sm:flex-row flex-col sm:justify-around items-start sm:gap-y-0 gap-y-4 sm:px-4 px-2">
         <JadwalSholat />
-        <JadwalSholat />
-        <JadwalSholat />
+        <MapCard />
+        <JamKerjaCard />
       </section>
     </main>
   );
